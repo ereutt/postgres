f4662f1f8f1990d6fc08628575f98ba27f6c78b2
Pavel Borisov <pashkin.elfe@gmail.com>
2026-02-05 14:42:37 +0400

    Fix merge with 5db6df0c0117

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -3140,7 +3140,7 @@ lmerge_matched:
 							 * the tuple moved, and setting our current
 							 * resultRelInfo to that.
 							 */
-							if (ItemPointerIndicatesMovedPartitions((ItemPointer) tupleid))
+							if (ItemPointerIndicatesMovedPartitions(&context->tmfd.ctid))
 								ereport(ERROR,
 										(errcode(ERRCODE_T_R_SERIALIZATION_FAILURE),
 										 errmsg("tuple to be merged was already moved to another partition due to concurrent update")));
@@ -3192,8 +3192,7 @@ lmerge_matched:
 									if (ItemPointerIsValid(&lockedtid))
 										UnlockTuple(resultRelInfo->ri_RelationDesc, &lockedtid,
 													InplaceUpdateTupleLock);
-									LockTuple(resultRelInfo->ri_RelationDesc,
-											  (ItemPointer) tupleid,
+									LockTuple(resultRelInfo->ri_RelationDesc, (ItemPointer) tupleid,
 											  InplaceUpdateTupleLock);
 									lockedtid = *((ItemPointer) tupleid);
 								}
